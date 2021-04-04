b673a302d726ee5163217e95a0baa900c374d76b
AMiracle-creator <65660523+AMiracle-creator@users.noreply.github.com>
2021-04-04 14:03:02 +0300

    Add files via upload

---
+++ crawler.py
@@ -0,0 +1,25 @@
+import time
+
+from graph_generator import generator_by_json
+from matrix_generator import matrix_from_graph
+from page_rank import summary_page_rank
+from services import get_page_links_v2
+
+# url = 'https://mangalib.me/otherworldly-evil-monarch?section=info'
+
+if __name__ == '__main__':
+    print('start')
+
+    URL = "https://www.baeldung.com/"
+
+    st = time.monotonic()
+
+    d = get_page_links_v2(URL, 1)
+
+    print(time.monotonic() - st)
+
+    generator_by_json(d)
+
+    matrix, height, width = matrix_from_graph()
+
+    summary_page_rank(matrix, height, width)
